4b1d685be4ff5251a9518ef00f19cc816f1e9c53
Krupskaia <krupskaia@outlook.com>
2022-07-29 10:45:53 -0700

    last

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,6 +1,25 @@
 // IMPORT THE MODULE
+import {add, subtract, multiply, divide} from "js/modules/calculator.js"
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+let number1 = Number(prompt('Enter the first number:'))
+let number2 = Number(prompt('Enter the second number:'))
+let operation = prompt("What operation would like to perform: (+, -, *, /) and store that in a variable.")
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+let result = 0
+switch(operation) {
+    case "+":
+        result = add(number1,number2)
+    case "-":
+        result = subtract(number1,number2)
+    case "*":
+        result = multiply(number1,number2)
+    case "/":
+        result = divide(number1,number2)
+    default:
+        alert("That is not an option")
+}
+
+alert(`The result of ${number1} ${operation} ${number2} is ${result}`)
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,31 @@
-// ADD A PRIVATE FUNCTION CALLED CALCULATE
+let result = 0
 
+// ADD A PRIVATE FUNCTION CALLED CALCULATE
+function calculate() {
+
+}
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 
+
 // ADD FUNCTION
+function add(number1,number2) {
+    result = number1 + number2
+}
 
 // SUBTRACT FUNCTION
+function subtract(number1,number2) {
+    result = number1 - number2
+}
 
 // MULTIPLY FUNCTION
+function multiply(number1,number2) {
+    result = number1 * number2
+}
 
 // DIVIDE FUNCTION
+function divide(number1,number2) {
+    result = number1 / number2
+}
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export {add, subtract, multiply, divide}
